5f4779b3c24be74fedcbce01f768b95118997a29
Aurick Qiao <qiao@aurick.net>
2016-07-12 08:13:26 -0700

    remove _build

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:48,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Bosen&#8217;s documentation!"],titleterms:{bosen:0,document:0,indice:0,tabl:0,welcom:0}})
