bec9b8bb6bf49bc977bffd836cc748aa285f0085
holyglenn <yuweirenywr@gmail.com>
2016-12-23 17:56:58 -0500

    HDFS support for Bosen in Cmake.

--- cmake/modules/FindHdfs.cmake
+++ cmake/modules/FindHdfs.cmake
@@ -1,26 +1,18 @@
 # - Try to find Hdfs
-#
-# The following variables are optionally searched for defaults
-#  HDFS_ROOT_DIR:            Base directory containing libhdfs.so 
-#
+##
 # The following are set after configuration is done:
 #  HDFS_LIBRARIES
+#  HDFS_INCLUDE_DIRS
+
+
+find_path(HDFS_INCLUDE_DIR NAMES hdfs.h  HINTS $ENV{HADOOP_HOME}/include)
+find_library(HDFS_LIBRARY NAMES hdfs HINTS $ENV{HADOOP_HOME}/lib/native)
 
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(HDFS DEFAULT_MSG
+        HDFS_INCLUDE_DIR HDFS_LIBRARY)
 
-#set(HDFS_ROOT_DIR "/usr/lib64")
-#set(JVM_ROOT_DIR "/usr/java/latest/jre/lib/amd64/server")
-
-#find_path(HDFS_INCLUDE_DIR yaml-cpp/yaml.h
-#        PATHS ${HDFS_ROOT_DIR})
-
-find_library(HDFS_LIBRARY hdfs)
-set(HDFS_LIBRARIES ${HDFS_LIBRARY})
-
-#find_package_handle_standard_args(HDFS DEFAULT_MSG
-#        HDFS_INCLUDE_DIR HDFS_LIBRARY)
-
-#if(HDFS_FOUND)
-#    set(HDFS_INCLUDE_DIRS ${HDFS_INCLUDE_DIR})
-#    set(HDFS_LIBRARIES ${HDFS_LIBRARY})
-#endif()
+if(HDFS_FOUND)
+    set(HDFS_INCLUDE_DIRS ${HDFS_INCLUDE_DIR})
+    set(HDFS_LIBRARIES ${HDFS_LIBRARY})
+endif()
--- cmake/modules/FindJvm.cmake
+++ cmake/modules/FindJvm.cmake
@@ -6,21 +6,13 @@
 # The following are set after configuration is done:
 #  JVM_LIBRARIES
 
+
+find_library(JAVA_LIBRARY NAMES jvm HINTS $ENV{JAVA_HOME}/jre/lib/amd64/server)
+
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(JVM DEFAULT_MSG
+        JAVA_LIBRARY)
 
-#set(JVM_ROOT_DIR "/usr/lib64")
-#set(JVM_ROOT_DIR "/usr/java/latest/jre/lib/amd64/server")
-
-#find_path(JVM_INCLUDE_DIR yaml-cpp/yaml.h
-#        PATHS ${JVM_ROOT_DIR})
-
-find_library(JVM_LIBRARY jvm)
-set(JVM_LIBRARIES ${JVM_LIBRARY})
-
-#find_package_handle_standard_args(JVM DEFAULT_MSG
-#        JVM_INCLUDE_DIR JVM_LIBRARY)
-
-#if(JVM_FOUND)
-#    set(JVM_INCLUDE_DIRS ${JVM_INCLUDE_DIR})
-#    set(JVM_LIBRARIES ${JVM_LIBRARY})
-#endif()
+if(JVM_FOUND)
+    set(JVM_LIBRARIES ${JAVA_LIBRARY})
+endif()
